c77cca96b8a99080d0b0631180146c897fffc295
rt_fan <rt_fan@mail.ru>
2024-04-27 10:56:54 +0300

    27.04.24

--- app.py
+++ app.py
@@ -8,39 +8,30 @@ log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
 
+# @app.route('/')
+# def index():
+#     with open("time.txt") as file:
+#         time_now = file.read()
+#
+#     with open("areas.txt") as file:
+#         a = file.read()
+#         areas = ast.literal_eval(a)
+#
+#     with open("employees.txt") as file:
+#         b = file.read()
+#         employees = ast.literal_eval(b)
+#
+#     names = sorted(employees)
+#     return render_template('index.html', names=names, areas=areas, employees=employees, time=time_now)
+
+
 @app.route('/')
-def index():
-    with open("time.txt") as file:
-        time_now = file.read()
-
-    with open("areas.txt") as file:
-        a = file.read()
-        areas = ast.literal_eval(a)
-
-    with open("employees.txt") as file:
-        b = file.read()
-        employees = ast.literal_eval(b)
-
-    names = sorted(employees)
-    return render_template('index.html', names=names, areas=areas, employees=employees, time=time_now)
-
-
-@app.route('/new')
 def index2():
     with open('data.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
         employees = data['employees']
         datetime_request = data['datetime']
         return render_template('index2.html', employees=employees, datetime_request=datetime_request)
-
-
-@app.route('/3')
-def index3():
-    with open('data.json', 'r', encoding='utf-8') as file:
-        data = json.load(file)
-        employees = data['employees']
-        datetime_request = data['datetime']
-        return render_template('index3.html', employees=employees, datetime_request=datetime_request)
 
 
 if __name__ == "__main__":
