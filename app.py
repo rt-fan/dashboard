e32ce0e5a71bd68be2e4adfcc8e7b1a66edf91a2
rtfan89 <rtfan89@gmail.com>
2025-04-16 06:16:01 +0000

    Динамически обновляемая страница

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, jsonify
-import ast
 import logging
 import json
 
@@ -14,26 +13,26 @@ def index():
         data = json.load(file)
         employees = data['employees']
         datetime_request = data['datetime']
-        return render_template('index.html', employees=employees, datetime_request=datetime_request)
+        return render_template('index_new.html', employees=employees, datetime_request=datetime_request)
 
 
-@app.route('/2')
-def index2():
-    return render_template('index2.html')
+@app.route('/api/data')
+def get_data():
+    """
+    API endpoint для получения данных из data.json.
+    """
+    try:
+        with open('data/data.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
     """
     Endpoint для проверки состояния приложения.
-    """
-    return jsonify({'status': 'OK'})
-
-
-@app.route('/getlogin', methods=['GET'])
-def get_login():
-    """
-    Возвращаю логин
     """
     return jsonify({'status': 'OK'})
 
